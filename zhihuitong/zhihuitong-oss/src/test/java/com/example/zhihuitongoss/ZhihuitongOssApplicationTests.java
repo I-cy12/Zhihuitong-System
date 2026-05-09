96bd26ac9251002be0c7828263d27b70b7e2ae77
Happy-boy-001 <3298755711@qq.com>
2026-05-09 11:50:30 +0800

    项目初始化

---
+++ zhihuitong/zhihuitong-oss/src/test/java/com/example/zhihuitongoss/ZhihuitongOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhihuitongoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhihuitongOssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
